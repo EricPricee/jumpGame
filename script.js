6f1b0184078a3376e6fa0a17eda36ba837adb5ef
EricPricee <eric.price121@gmail.com>
2022-11-03 14:07:42 -0400

    gb Push

---
+++ script.js
@@ -0,0 +1,28 @@
+var characater =
+document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+    if(character.classList != "animate"){
+    characater.classList.add("animate");
+    }
+
+    setTimeout(function(){
+        character.classList.remove("animate");
+},500);
+}
+
+var checkDead = setInterval(function(){
+    var characterTop =
+    parseInt(window.getComputedStyle(character).
+    getPropertyValue("top"));
+    var blockLeft =
+    parseInt(window.getComputedStyle(block).
+    getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 &&
+        characterTop>=130){
+            block.style.animation ="none";
+            block.style.display = "none";
+            alert("😈 hahaaaaaaaa you lost! 👺.")
+        }
+},10);
